4548dfb0a6c3c517c24ead663dbc3f7952417210
YanFang <838641570@qq.com>
2020-05-11 09:29:16 +0800

    url-loader,参数配置

--- webpack.config.js
+++ webpack.config.js
@@ -17,13 +17,23 @@ module.exports = {
                 test: /\.scss$/,
                 use:["style-loader", "css-loader", "sass-loader"]
             },{
-                test: /\.jpg$/,
+                test: /\.(jpg|png|jpeg)$/,
                 // use: [
                 //     {
                 //         loader: "file-loader"
                 //     }
                 // ]
-                loader: "file-loader"
+
+                // loader: "file-loader"
+
+                use: {
+                    loader: "url-loader",
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'image/',
+                        limit: 10240
+                    }
+                }
             }
         ]
     }
