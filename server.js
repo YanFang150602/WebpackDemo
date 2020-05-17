6873b31bab1c9e392260fd8210a934d59eaef1da
YanFang <838641570@qq.com>
2020-05-17 14:11:40 +0800

    express.static('express')

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const WebpackDevMiddleWare = require('webpack-dev-middleware');
 const webpack = require('webpack');
 const config = require('./webpack.config.js');
@@ -6,10 +7,12 @@ const config = require('./webpack.config.js');
 const compiler = webpack(config);
 const app = express();
 
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
 app.use(WebpackDevMiddleWare(compiler, {
     publicPath:'/'
 }));
 
 app.listen('3000', () => {
     console.log('server is running!!!');
-})
+});
